4af25141a227eb1bfc8215751a689dc6d97c5e37
marksteelz3 <31002690+marksteelz3@users.noreply.github.com>
2019-07-01 23:56:35 -0400

    Update with memory research project scenes
    Under Assets/Scenes/ME555_Memory Experiment are the scenes used for my research project on the effects of visual stimulus on memory peformance

---
+++ Assets/ShowMeaning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMeaning : MonoBehaviour {
+    public GameObject term;
+
+	// Update is called once per frame
+	void Update () {
+		if (term.transform.childCount > 3)
+        {
+            this.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
---
+++ Assets/TrialTimer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrialTimer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//textComponent = GetComponentInChildren<VRTK_ObjectTooltip > ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
